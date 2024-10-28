628e7c3c173f9c66f9c1f0e11c1c2fb7ae0846cd
ten921336 <138644194+ten921336@users.noreply.github.com>
2024-10-28 16:11:34 +0900

    Update main.js

--- main.js
+++ main.js
@@ -254,20 +254,14 @@ function init() {
     return raycaster.intersectObjects(group.children, false);
   }
 
-  // コントローラーイベントの処理
-  // function handleController(controller) {
-  //   const userData = controller.userData;
-  //   if (userData.isSelecting === true) {
-  //     // コントローラーボタンが押された際の処理
-  //     const cube = createCube();
-  //     cube.position.set(
-  //       Math.random() * -1000 - 300,  // x座標を-5から5の範囲でランダムに設定
-  //       0,  // y座標
-  //       Math.random() * -1000 - 300   // z座標を-5から5の範囲でランダムに設定
-  //     );
-  //     scene.add(cube);
-  //   }
-  // }
+  //コントローラーイベントの処理
+  function handleController(controller) {
+    const userData = controller.userData;
+    if (userData.isSelecting === true) {
+      // コントローラーボタンが押された際の処理
+      console.log("AAAAA");
+    }
+  }
 
   /* -------------------コントローラー設定 ここまで------------------- */
 
